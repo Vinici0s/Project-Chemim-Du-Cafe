1b4ad6c1caac4c4ddea11247cff1aa13b5f6e2d0
Vinicios Albuquerque <viniciusmelo.art@gmail.com>
2022-09-05 01:27:13 -0300

    ajustes no script e menu de linguagem

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ backButton = document.getElementById("backTop");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     backButton.style.display = "block";
   } else {
     backButton.style.display = "none";
@@ -13,6 +13,24 @@ function scrollFunction() {
 }
 
 function backTopFunction() {
-//   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0;
+
+  currentYOffset = self.pageYOffset;
+  initYOffset = currentYOffset;
+
+  var intervalId = setInterval(function(){
+  currentYOffset -= initYOffset*0.04; 
+  document.body.scrollTop = currentYOffset ;
+  document.documentElement.scrollTop = currentYOffset;
+
+    if(self.pageYOffset === 0){
+      clearInterval(intervalId);
+    }
+  }, 20);
+  
 }
+
+/*    Prevent Default    */
+
+document.addEventListener('click', function(event){
+  const element = event.preventDefault();
+});
